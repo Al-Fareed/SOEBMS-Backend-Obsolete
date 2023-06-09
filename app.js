6602ec7441931e5bd7cac098315a13b8f38dc5de
Al-Fareed <alfareedss472@gmail.com>
2023-06-09 22:55:59 +0530

    just started

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-app.use((req,res,next)=>{
-  console.log('Hello bro');
-  
-  next();
-})
-app.use((req, res, next) => {
-  res.send('Hello, world!');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/user", (req, res, next) => {
+  res.send("<h1>User: " + req.body.username + "</h1>");
 });
+
+app.get("/", (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Add</button></form>'
+  );
+});
+
 app.listen(5000);
